d98a02c9e0e7ee70d0a2eb124416ab4898703b8e
Yyyyyy T.T <thiams@rpi.edu>
2024-03-26 21:28:58 -0400

    built the structure of the app, and created descriptions for each function for the back end chat augmentation

---
+++ chat_interface/backend_modules/get_keywords.py
@@ -0,0 +1,7 @@
+'''
+Takes user input which can be a fairly long string, and returns
+a list of a few(1 to 5) of the key words(strings) found in the string in order
+to help with search querying
+'''
+def get_keywords(user_input:str) -> list:
+    pass
---
+++ chat_interface/backend_modules/get_urls.py
@@ -0,0 +1,12 @@
+'''
+takes as input an array of key words, combines them into a single string,
+and then searches the web to find 1-5 urls that match the most with
+the keywords provided.
+
+The URLs should be returned as a list of strings.
+
+The main websites we will be searching for now will be bloomberg, yahoo finance, and 
+google search. If you want, you can use other sites in your implementation
+'''
+def get_urls(keywords:list) -> list:
+    pass
