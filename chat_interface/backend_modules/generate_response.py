109ed9c8935b3d206bf518e25f7a09ee180c3c1f
Wunzir <abdelgadirm01@gmail.com>
2024-05-07 16:02:09 -0400

    Finished generating responses module

--- chat_interface/backend_modules/generate_response.py
+++ chat_interface/backend_modules/generate_response.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+import openai
 '''
 Use GPT-3.5 API to generate response from the user input. We will be 
 using "Retrieval Augmented Generation" to add the context of the scraping 
@@ -6,5 +9,16 @@ the "get_urls" and "clean_urls" modules will return.
 Input will be a string(user input) and a list of maps(data) of the form {URL:text}.
 The output should be a string.
 '''
-def generate_response(user_input:str, data:list) -> str:
+def generate_response(user_input:str, data:dict) -> str:
+    load_dotenv()
+    openai.api_key = os.getenv("API_KEY7")
+    # Using GPT 3.5, generate a response based on the user's request
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [{"role": "user", "content": user_input}] + [{"role": "system", "content": data[url]} for url in data]
+    )
+    print(completion.choices[0].message.content)
+    return completion.choices[0].message.content
+
+if __name__ == "__main__":
     pass
