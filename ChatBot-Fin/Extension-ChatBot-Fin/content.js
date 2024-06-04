9e1aa3b1ca6009785108a6657fae3c62795d838c
Felix Tian <tianf2@rpi.edu>
2024-06-04 17:39:20 -0400

    Refactor #3

--- ChatBot-Fin/Extension-ChatBot-Fin/content.js
+++ ChatBot-Fin/Extension-ChatBot-Fin/content.js
@@ -19,218 +19,171 @@ fetch(`http://127.0.0.1:8000/input_webtext/?textContent=${encodedContent}`, { me
         console.error('There was a problem with your fetch operation:', error);
     });
 
-
-  
 function clear() {
-    const response = document.getElementById('respons');
-    const sourceurls = document.getElementById('source_urls');
+    var response = document.getElementById('respons');
+    var sourceurls = document.getElementById('source_urls');
 
     response.innerHTML = "";
-  sourceurls.innerHTML = "";
-  fetch(`http://127.0.0.1:8000/clear_messages/`, { method: "POST" })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('There was a problem with your fetch operation:', error);
-    });
-
-
+    sourceurls.innerHTML = "";
+    fetch(`http://127.0.0.1:8000/clear_messages/`, { method: "POST" })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('There was a problem with your fetch operation:', error);
+        });
 }
-
-
-//making text box for popup
-const textbox = document.createElement("input");
-textbox.id = "textbox";
 
 function get_chat_response(question) {
+    var response = document.getElementById('respons');
 
-  
-  // Define the 'response' element 
-    const response = document.getElementById('respons');
-
-    // Create a 'span' element to display the user's question
-    const your_question = document.createElement('span');
+    var your_question = document.createElement('span');
     your_question.innerText = "You: " + question + "\n";
-  response.appendChild(your_question);
+    response.appendChild(your_question);
 
-  // Create a 'span' element for the loading message
-    const loading = document.createElement('span');
+    var loading = document.createElement('span');
     loading.innerText = "FinGPT: Loading...";
-  document.getElementById("respons").appendChild(loading);
+    document.getElementById("respons").appendChild(loading);
 
-  // Create the 'request' object
-    const request = {"question": question};
+    var request = {"question": question};
     document.getElementById("respons").scrollTop = document.getElementById("respons").scrollHeight;
-  
 
-  question = encodeURI(question);
+    question = encodeURI(question);
 
+    fetch(`http://127.0.0.1:8000/get_chat_response/?question=${question}`, {method: "GET"})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data["resp"]);
 
-  fetch(`http://127.0.0.1:8000/get_chat_response/?question=${question}`, {method: "GET"})
-    .then(response => response.json())
-    .then(data => {
-      console.log(data["resp"]);
+            response.removeChild(loading);
 
-      // Log the URL to the console
-      
-
-      // Remove the loading message
-      response.removeChild(loading);
-      
-      // Append the response to the 'response' element
-      resptext = document.createElement('span');
-      resptext.innerText = "FinGPT: " + data["resp"] + "\n";
-      document.getElementById("respons").appendChild(resptext);
-      textbox.value = "";
-      document.getElementById("respons").scrollTop = document.getElementById("respons").scrollHeight;
-    })
-
+            var resptext = document.createElement('span');
+            resptext.innerText = "FinGPT: " + data["resp"] + "\n";
+            document.getElementById("respons").appendChild(resptext);
+            textbox.value = "";
+            document.getElementById("respons").scrollTop = document.getElementById("respons").scrollHeight;
+        });
 }
 
-let searchQuery = "";
-
-let popup = document.createElement('span');
-popup.id = "draggableElement";
-
-//making button for sources
-const sources = document.createElement('span');
-sources.id = "sources";
-sources.innerText = "Sources";
-sources.onclick = function() {get_sources(searchQuery)};
-
+var searchQuery = ""
 function get_adv_chat_response(question) {
-  
-  popup.appendChild(sources);
-  // Define the 'response' element 
-    const response = document.getElementById('respons');
+    popup.appendChild(sources);
+    var response = document.getElementById('respons');
     searchQuery = question
-  // Create a 'span' element to display the user's question
-    const your_question = document.createElement('span');
+
+    var your_question = document.createElement('span');
     your_question.innerText = "You: " + question + "\n";
-  response.appendChild(your_question);
+    response.appendChild(your_question);
 
-  // Create a 'span' element for the loading message
-    const loading = document.createElement('span');
+    var loading = document.createElement('span');
     loading.innerText = "FinGPT: Loading...";
-  document.getElementById("respons").appendChild(loading);
+    document.getElementById("respons").appendChild(loading);
 
-  // Create the 'request' object
-    const request = {"question": question};
+    var request = {"question": question};
     document.getElementById("respons").scrollTop = document.getElementById("respons").scrollHeight;
 
+    fetch(`http://127.0.0.1:8000/get_adv_response/?question=${question}`, {method: "GET"})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data["resp"]);
 
-  
+            response.removeChild(loading);
 
-
-  fetch(`http://127.0.0.1:8000/get_adv_response/?question=${question}`, {method: "GET"})
-    .then(response => response.json())
-    .then(data => {
-      console.log(data["resp"]);
-
-      
-      
-      
-      
-      // Remove the loading message
-      response.removeChild(loading);
-      
-      // Append the response to the 'response' element
-      resptext = document.createElement('span');
-      resptext.innerText = "FinGPT: " + data["resp"] + "\n";
-      document.getElementById("respons").appendChild(resptext);
-      textbox.value = "";
-      document.getElementById("respons").scrollTop = document.getElementById("respons").scrollHeight;
-    })
-
+            var resptext = document.createElement('span');
+            resptext.innerText = "FinGPT: " + data["resp"] + "\n";
+            document.getElementById("respons").appendChild(resptext);
+            textbox.value = "";
+            document.getElementById("respons").scrollTop = document.getElementById("respons").scrollHeight;
+        });
 }
-
-const sitebody = document.body;
-
-const sources_window = document.createElement('span');
-sources_window.className = "sources-window";
 
 function get_sources(search_query) {
-  sources_window.style.display = 'block';
-  console.log(search_query);
+    sources_window.style.display = 'block';
+    console.log(search_query);
 
-  sitebody.append(sources_window);
-    const response = document.getElementById('respons');
-    console.log("Getting sources");
+    sitebody.append(sources_window);
+    var response = document.getElementById('respons');
+    console.log("hi1");
 
-  fetch(`http://127.0.0.1:8000/get_source_urls/?query=${String(search_query)}`, { method: "GET" })
-      .then(response => response.json())
-      .then(data => {
-          console.log(data["resp"]);
+    fetch(`http://127.0.0.1:8000/get_source_urls/?query=${String(search_query)}`, { method: "GET" })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data["resp"]);
+            console.log("HIIII2");
+            const sources = data["resp"];
+            var source_urls = document.getElementById('source_urls');
+            source_urls.innerText = '';
 
-          console.log("Fetching...");
-          const sources = data["resp"];
-          const source_urls = document.getElementById('source_urls');
-          source_urls.innerText = '';
-          // Loop through each source and create a clickable link
-          sources.forEach(source => {
-              const url = source[0];
-              const icon_url = source[1];
-              const link = document.createElement('a');
-              link.href = url;
-          link.innerText = url;
-          link.target = "_blank"; // Open link in a new tab
+            sources.forEach(source => {
+                var url = source[0];
+                var icon_url = source[1];
+                var link = document.createElement('a');
+                link.href = url;
+                link.innerText = url;
+                link.target = "_blank";
 
-          // Create a container div for the icon and the link
-              const container = document.createElement('div');
-              container.style.display = "flex"; // Use flexbox for layout
-          container.style.alignItems = "center"; // Center items vertically
+                var container = document.createElement('div');
+                container.style.display = "flex";
+                container.style.alignItems = "center";
 
-          // Create an image element for the icon
-              const icon = document.createElement('img');
-              icon.src = icon_url;
-          icon.alt = "Icon";
-          icon.style.width = "16px"; // Set the icon size
-          icon.style.height = "16px";
-          icon.style.marginRight = "5px"; // Add some spacing between the icon and the link
+                var icon = document.createElement('img');
+                icon.src = icon_url;
+                icon.alt = "Icon";
+                icon.style.width = "16px";
+                icon.style.height = "16px";
+                icon.style.marginRight = "5px";
 
-    // Append the icon and the link to the container
-    container.appendChild(icon);
-    container.appendChild(link);
+                container.appendChild(icon);
+                container.appendChild(link);
 
-    // Append the container to the source_urls element
-    source_urls.appendChild(container);
-});
-
-      });
+                source_urls.appendChild(container);
+            });
+        });
 }
 
+var sitebody = document.body;
 
-let text = document.createElement('span');
+var popup = document.createElement('span');
+popup.id = "draggableElement";
+
+var text = document.createElement('span');
 text.id = "text";
 text.innerText = "Ask me question.";
 
-let response = document.createElement('span');
+var response = document.createElement('span');
 response.id = "respons";
 response.innerText = "";
 
-let confirm_button = document.createElement('span');
+var textbox = document.createElement("input");
+textbox.id = "textbox";
+
+var confirm_button = document.createElement('span');
 confirm_button.id = "confirm_button";
 confirm_button.innerText = "Ask";
 confirm_button.onclick = function() {get_chat_response(textbox.value)};
 
-let clear_button = document.createElement('span');
+var clear_button = document.createElement('span');
 clear_button.id = "clear_button";
 clear_button.innerText = "Clear";
 clear_button.onclick = function() {clear()};
 
-let adv_button = document.createElement('span');
+var adv_button = document.createElement('span');
 adv_button.id = "adv_button";
 adv_button.innerText = "Advanced Ask";
 adv_button.onclick = function() {get_adv_chat_response(textbox.value)};
 
-let exit_sources = document.createElement('span');
+var sources = document.createElement('span');
+sources.id = "sources";
+sources.innerText = "Sources";
+sources.onclick = function() {get_sources(searchQuery)};
+
+var exit_sources = document.createElement('span');
 exit_sources.id = "exit_sources";
 exit_sources.innerText = "Exit";
 exit_sources.onclick = function() { sources_window.style.display = 'none'; };
@@ -242,69 +195,61 @@ popup.appendChild(text);
 popup.appendChild(response);
 popup.appendChild(adv_button);
 
+var sources_window = document.createElement('span');
+sources_window.className = "sources-window";
 
-let source_urls = document.createElement('span');
+var source_urls = document.createElement('span');
 source_urls.id = "source_urls";
 
 sources_window.appendChild(source_urls);
 sources_window.appendChild(exit_sources);
 
-sitebody.appendChild(popup)
-//sitebody.append(sources_window)
+sitebody.appendChild(popup);
 
-popup = document.getElementById("draggableElement");
-let isDragging = false;
-let offsetX, offsetY;
+var popup = document.getElementById("draggableElement");
+var isDragging = false;
+var offsetX, offsetY;
 
-// Function to handle the mouse down event
 function onMouseDown(event) {
-  isDragging = true;
-  offsetX = event.clientX - popup.getBoundingClientRect().left;
-  offsetY = event.clientY - popup.getBoundingClientRect().top;
+    isDragging = true;
+    offsetX = event.clientX - popup.getBoundingClientRect().left;
+    offsetY = event.clientY - popup.getBoundingClientRect().top;
 }
 
-// Function to handle the mouse move event
 function onMouseMove(event) {
-  if (isDragging) {
-      const newX = event.clientX - offsetX;
-      const newY = event.clientY - offsetY;
+    if (isDragging) {
+        var newX = event.clientX - offsetX;
+        var newY = event.clientY - offsetY;
 
-      popup.style.left = newX + "px";
-    popup.style.top = newY + "px";
-  }
+        popup.style.left = newX + "px";
+        popup.style.top = newY + "px";
+    }
 }
 
-// Function to handle the mouse up event
 function onMouseUp() {
-  isDragging = false;
+    isDragging = false;
 }
 
-// Add event listeners for mouse events
 popup.addEventListener("mousedown", onMouseDown);
 document.addEventListener("mousemove", onMouseMove);
 document.addEventListener("mouseup", onMouseUp);
 
 document.addEventListener('mousemove', function(e) {
-  if (isDragging) {
-      const newX = e.clientX - offsetX;
-      const newY = e.clientY - offsetY;
+    if (isDragging) {
+        var newX = e.clientX - offsetX;
+        var newY = e.clientY - offsetY;
 
-      popup.style.left = newX + "px";
-    popup.style.top = newY + "px";
+        popup.style.left = newX + "px";
+        popup.style.top = newY + "px";
 
-    // Update the position of the sources window
-      const sourcesWindowLeft = newX + parseInt(popup.style.width) + 10;
-      const sourcesWindowTop = newY;
-      sources_window.style.left = sourcesWindowLeft + "px";
-    sources_window.style.top = sourcesWindowTop + "px";
-  }
+        var sourcesWindowLeft = newX + parseInt(popup.style.width) + 10;
+        var sourcesWindowTop = newY;
+        sources_window.style.left = sourcesWindowLeft + "px";
+        sources_window.style.top = sourcesWindowTop + "px";
+    }
 });
-
 
 function respond() {
     question = textbox.value;
     response.innerText = "You asked: " + question;
 }
-
-
-
