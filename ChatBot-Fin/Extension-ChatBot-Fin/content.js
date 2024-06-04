9fd06e154b22aa4db88adf743401f974bb426e70
Felix Tian <tianf2@rpi.edu>
2024-06-04 18:49:23 -0400

    Refactor #4
    Completely new look

--- ChatBot-Fin/Extension-ChatBot-Fin/content.js
+++ ChatBot-Fin/Extension-ChatBot-Fin/content.js
@@ -20,8 +20,8 @@ fetch(`http://127.0.0.1:8000/input_webtext/?textContent=${encodedContent}`, { me
     });
 
 function clear() {
-    var response = document.getElementById('respons');
-    var sourceurls = document.getElementById('source_urls');
+    const response = document.getElementById('respons');
+    const sourceurls = document.getElementById('source_urls');
 
     response.innerHTML = "";
     sourceurls.innerHTML = "";
@@ -40,18 +40,20 @@ function clear() {
         });
 }
 
-function get_chat_response(question) {
-    var response = document.getElementById('respons');
+const textbox = document.createElement("input");
 
-    var your_question = document.createElement('span');
+function get_chat_response(question) {
+    const response = document.getElementById('respons');
+
+    const your_question = document.createElement('span');
     your_question.innerText = "You: " + question + "\n";
     response.appendChild(your_question);
 
-    var loading = document.createElement('span');
+    const loading = document.createElement('span');
     loading.innerText = "FinGPT: Loading...";
     document.getElementById("respons").appendChild(loading);
 
-    var request = {"question": question};
+    const request = {"question": question};
     document.getElementById("respons").scrollTop = document.getElementById("respons").scrollHeight;
 
     question = encodeURI(question);
@@ -63,7 +65,7 @@ function get_chat_response(question) {
 
             response.removeChild(loading);
 
-            var resptext = document.createElement('span');
+            const resptext = document.createElement('span');
             resptext.innerText = "FinGPT: " + data["resp"] + "\n";
             document.getElementById("respons").appendChild(resptext);
             textbox.value = "";
@@ -71,21 +73,24 @@ function get_chat_response(question) {
         });
 }
 
-var searchQuery = ""
+let searchQuery = "";
+
+let popup;
+
 function get_adv_chat_response(question) {
     popup.appendChild(sources);
-    var response = document.getElementById('respons');
+    const response = document.getElementById('respons');
     searchQuery = question
 
-    var your_question = document.createElement('span');
+    const your_question = document.createElement('span');
     your_question.innerText = "You: " + question + "\n";
     response.appendChild(your_question);
 
-    var loading = document.createElement('span');
+    const loading = document.createElement('span');
     loading.innerText = "FinGPT: Loading...";
     document.getElementById("respons").appendChild(loading);
 
-    var request = {"question": question};
+    const request = {"question": question};
     document.getElementById("respons").scrollTop = document.getElementById("respons").scrollHeight;
 
     fetch(`http://127.0.0.1:8000/get_adv_response/?question=${question}`, {method: "GET"})
@@ -95,7 +100,7 @@ function get_adv_chat_response(question) {
 
             response.removeChild(loading);
 
-            var resptext = document.createElement('span');
+            const resptext = document.createElement('span');
             resptext.innerText = "FinGPT: " + data["resp"] + "\n";
             document.getElementById("respons").appendChild(resptext);
             textbox.value = "";
@@ -103,12 +108,14 @@ function get_adv_chat_response(question) {
         });
 }
 
+const sitebody = document.body;
+
 function get_sources(search_query) {
     sources_window.style.display = 'block';
     console.log(search_query);
 
     sitebody.append(sources_window);
-    var response = document.getElementById('respons');
+    const response = document.getElementById('respons');
     console.log("hi1");
 
     fetch(`http://127.0.0.1:8000/get_source_urls/?query=${String(search_query)}`, { method: "GET" })
@@ -117,22 +124,22 @@ function get_sources(search_query) {
             console.log(data["resp"]);
             console.log("HIIII2");
             const sources = data["resp"];
-            var source_urls = document.getElementById('source_urls');
+            const source_urls = document.getElementById('source_urls');
             source_urls.innerText = '';
 
             sources.forEach(source => {
-                var url = source[0];
-                var icon_url = source[1];
-                var link = document.createElement('a');
+                const url = source[0];
+                const icon_url = source[1];
+                const link = document.createElement('a');
                 link.href = url;
                 link.innerText = url;
                 link.target = "_blank";
 
-                var container = document.createElement('div');
+                const container = document.createElement('div');
                 container.style.display = "flex";
                 container.style.alignItems = "center";
 
-                var icon = document.createElement('img');
+                const icon = document.createElement('img');
                 icon.src = icon_url;
                 icon.alt = "Icon";
                 icon.style.width = "16px";
@@ -147,68 +154,64 @@ function get_sources(search_query) {
         });
 }
 
-var sitebody = document.body;
 
-var popup = document.createElement('span');
+popup = document.createElement('div');
 popup.id = "draggableElement";
 
-var text = document.createElement('span');
-text.id = "text";
-text.innerText = "Ask me question.";
+const header = document.createElement('div');
+header.id = "header";
 
-var response = document.createElement('span');
-response.id = "respons";
-response.innerText = "";
+const title = document.createElement('span');
+title.innerText = "FinGPT";
 
-var textbox = document.createElement("input");
-textbox.id = "textbox";
+// Used for Dark mode and other settings, not implemented yet
+const settingsIcon = document.createElement('span');
+settingsIcon.innerText = "⚙️";
+settingsIcon.className = "icon";
 
-var confirm_button = document.createElement('span');
-confirm_button.id = "confirm_button";
-confirm_button.innerText = "Ask";
-confirm_button.onclick = function() {get_chat_response(textbox.value)};
+const closeIcon = document.createElement('span');
+closeIcon.innerText = "❌";
+closeIcon.className = "icon";
+closeIcon.onclick = function() { popup.style.display = 'none'; };
 
-var clear_button = document.createElement('span');
-clear_button.id = "clear_button";
-clear_button.innerText = "Clear";
-clear_button.onclick = function() {clear()};
+header.appendChild(title);
+header.appendChild(settingsIcon);
+header.appendChild(closeIcon);
 
-var adv_button = document.createElement('span');
-adv_button.id = "adv_button";
-adv_button.innerText = "Advanced Ask";
-adv_button.onclick = function() {get_adv_chat_response(textbox.value)};
+const content = document.createElement('div');
+content.id = "content";
 
-var sources = document.createElement('span');
-sources.id = "sources";
-sources.innerText = "Sources";
-sources.onclick = function() {get_sources(searchQuery)};
+const titleText = document.createElement('h2');
+titleText.innerText = "Your personalized financial assistant.";
 
-var exit_sources = document.createElement('span');
-exit_sources.id = "exit_sources";
-exit_sources.innerText = "Exit";
-exit_sources.onclick = function() { sources_window.style.display = 'none'; };
+const subtitleText = document.createElement('p');
+subtitleText.innerText = "Here are some quick functions to get you started:";
 
-popup.appendChild(textbox);
-popup.appendChild(confirm_button);
-popup.appendChild(clear_button);
-popup.appendChild(text);
-popup.appendChild(response);
-popup.appendChild(adv_button);
+content.appendChild(titleText);
+content.appendChild(subtitleText);
 
-var sources_window = document.createElement('span');
-sources_window.className = "sources-window";
+const inputContainer = document.createElement('div');
+inputContainer.id = "inputContainer";
 
-var source_urls = document.createElement('span');
-source_urls.id = "source_urls";
+textbox.type = "text";
+textbox.placeholder = "Ask me something!";
 
-sources_window.appendChild(source_urls);
-sources_window.appendChild(exit_sources);
+const sendButton = document.createElement('button');
+sendButton.innerText = "➡️";
+sendButton.onclick = function() { get_chat_response(textbox.value) };
+
+inputContainer.appendChild(textbox);
+inputContainer.appendChild(sendButton);
+
+popup.appendChild(header);
+popup.appendChild(content);
+popup.appendChild(inputContainer);
 
 sitebody.appendChild(popup);
 
-var popup = document.getElementById("draggableElement");
-var isDragging = false;
-var offsetX, offsetY;
+popup = document.getElementById("draggableElement");
+let isDragging = false;
+let offsetX, offsetY;
 
 function onMouseDown(event) {
     isDragging = true;
@@ -218,8 +221,8 @@ function onMouseDown(event) {
 
 function onMouseMove(event) {
     if (isDragging) {
-        var newX = event.clientX - offsetX;
-        var newY = event.clientY - offsetY;
+        const newX = event.clientX - offsetX;
+        const newY = event.clientY - offsetY;
 
         popup.style.left = newX + "px";
         popup.style.top = newY + "px";
@@ -236,16 +239,11 @@ document.addEventListener("mouseup", onMouseUp);
 
 document.addEventListener('mousemove', function(e) {
     if (isDragging) {
-        var newX = e.clientX - offsetX;
-        var newY = e.clientY - offsetY;
+        const newX = e.clientX - offsetX;
+        const newY = e.clientY - offsetY;
 
         popup.style.left = newX + "px";
         popup.style.top = newY + "px";
-
-        var sourcesWindowLeft = newX + parseInt(popup.style.width) + 10;
-        var sourcesWindowTop = newY;
-        sources_window.style.left = sourcesWindowLeft + "px";
-        sources_window.style.top = sourcesWindowTop + "px";
     }
 });
 
