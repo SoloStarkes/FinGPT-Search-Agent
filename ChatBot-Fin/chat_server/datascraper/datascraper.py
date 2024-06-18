6825f969480136f8bbe39ba0437e77346d9306b9
Felix Tian <tianf2@rpi.edu>
2024-06-18 18:55:42 -0400

    Update datascraper.py
    Formatting

--- ChatBot-Fin/chat_server/datascraper/datascraper.py
+++ ChatBot-Fin/chat_server/datascraper/datascraper.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
-import time 
-
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -16,15 +16,13 @@ from urllib.parse import urljoin
 api_key = os.getenv("API_KEY7")
 
 
-    
 def data_scrape(url, timeout=1):
-
     try:
         start_time = time.time()
         response = requests.get(url, timeout=timeout)
         end_time = time.time()
         elapsed_time = end_time - start_time
-        
+
         if response.status_code == 200:
             print("Successful response")
             if elapsed_time > timeout:
@@ -32,7 +30,6 @@ def data_scrape(url, timeout=1):
                 return -1
             soup = BeautifulSoup(response.text, 'html.parser')
             first_5000_characters = soup.text[:4000]
-        
 
             return first_5000_characters
         else:
@@ -46,8 +43,6 @@ def data_scrape(url, timeout=1):
         return -1
 
 
-
-
 def search_websites_with_keyword(keyword):
     # Define the search query
     search_query = f"intitle:{keyword}"  # This query searches for "rpi" in the title of web pages
@@ -56,7 +51,7 @@ def search_websites_with_keyword(keyword):
     search_url = f"https://www.google.com/search?q={search_query}"
     headers = {"User-Agent": "Your User Agent Here"}  # Replace with your User-Agent header
     response = requests.get(search_url, headers=headers)
-    
+
     # Check if the request was successful
     if response.status_code == 200:
         # Parse the HTML content of the search results page
@@ -71,11 +66,7 @@ def search_websites_with_keyword(keyword):
                 url = link[7:]  # Remove "/url?q=" prefix
                 info = data_scrape(url)
                 if (info != -1):
-                    message_list.append( {"role": "system", "content": info})
-
-                
-
-
+                    message_list.append({"role": "system", "content": info})
 
                 # You can further process this URL or send a request to scrape data from the website
 
@@ -83,74 +74,63 @@ def search_websites_with_keyword(keyword):
         print("Failed to retrieve search results.")
 
 
-
-
-
-
-
 def create_advanced_response(user_input, message_list):
-    
     print(message_list)
     openai.api_key = api_key
-    print("starting creation") 
+    print("starting creation")
     #urls = get_goog_urls(user_input)
-    for url in search(user_input, num=10, stop=10, pause =0):
+    for url in search(user_input, num=10, stop=10, pause=0):
         info = data_scrape(url)
         if (info != -1):
             print(info)
-            message_list.append( {"role": "system", "content": "url: " + str(url) + "info: " + info})
-    
+            message_list.append({"role": "system", "content": "url: " + str(url) + "info: " + info})
 
-    message_list.append( {"role": "user", "content": user_input}) 
+    message_list.append({"role": "user", "content": user_input})
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=message_list,
-        
+
     )
     print(completion.choices[0].message.content)
-   
-    
+
     return completion.choices[0].message.content
 
 
 def create_response(user_input, message_list):
     print(message_list)
     openai.api_key = api_key
-    print("starting creation") 
-    
-    
+    print("starting creation")
 
-    message_list.append( {"role": "user", "content": user_input}) 
+    message_list.append({"role": "user", "content": user_input})
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=message_list,
-        
+
     )
 
     print(completion.choices[0].message.content)
-   
+
     message_list.append({"role": "system", "content": completion.choices[0].message.content})
 
     return completion.choices[0].message.content
 
-# def clear(request): 
+
+# def clear(request):
 #     message_list = []
 #     print("cleared")
 #     return 
 
 def get_sources(query):
-
-  
     sources = []
-    for url in search(query, num=10, stop=10, pause =0):
+    for url in search(query, num=10, stop=10, pause=0):
         info = data_scrape(url)
         if (info != -1):
             tup = url, get_website_icon(url)
             sources.append(tup)
             print(url)
     return sources
-    
+
 
 # def get_goog_urls(search_query):
 #     urls = []
@@ -159,7 +139,7 @@ def get_sources(query):
 #         urls.append(url)
 #     return urls 
 
-    
+
 def get_website_icon(url):
     # Send a GET request to the URL
     response = requests.get(url)
